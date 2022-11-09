b6f4e389f5a94b0edf2dd0e592db2ea3f75790f9
sanjitkumer <sanjitk666@gmail.com>
2022-11-10 00:35:08 +0600

    order get part

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         const serviceCollection = client.db('teachingServices').collection('services');
-        const orderCollection = client.db('teachingServices').collection('services')
+        const orderCollection = client.db('teachingServices').collection('orders')
 
         app.get('/services', async(req,res) => {
             const query ={}
@@ -36,6 +36,20 @@ async function run(){
 
 
         //orders api
+        app.get('/orders', async(req, res) => {
+            // console.log(req,query.email);
+            let query = {};
+            if(req.query.email){
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
+
+
         app.post('/orders', async(req, res) =>{
             const order = req.body;
             const result = await orderCollection.insertOne(order);
