c9583d277c09f2a5d9bae89eeec4fd82b4a2c2a5
sanjitkumer <sanjitk666@gmail.com>
2022-11-09 22:57:30 +0600

    added post part

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 
@@ -18,6 +18,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         const serviceCollection = client.db('teachingServices').collection('services');
+        const orderCollection = client.db('teachingServices').collection('services')
 
         app.get('/services', async(req,res) => {
             const query ={}
@@ -25,6 +26,24 @@ async function run(){
             const services = await cursor.toArray();
             res.send(services);
         });
+
+        app.get('/services/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+
+
+        //orders api
+        app.post('/orders', async(req, res) =>{
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        })
+
+
+
 
     }
     finally{
