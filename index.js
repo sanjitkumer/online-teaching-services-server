7d419f57c7a262597dc3aa1aa7b222cbc88554df
sanjitkumer <sanjitk666@gmail.com>
2022-11-09 22:21:36 +0600

    added get part

--- index.js
+++ index.js
@@ -16,6 +16,20 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
+    try{
+        const serviceCollection = client.db('teachingServices').collection('services');
+
+        app.get('/services', async(req,res) => {
+            const query ={}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+    }
+    finally{
+
+    }
 
 }
 
